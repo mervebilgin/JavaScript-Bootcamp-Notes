70a4d03d87c30fc5a6301451e907d712baf06da3
bilginmrv <mrvblgn44@gmail.com>
2023-01-31 16:40:52 +0300

    What is javascript?

--- 1-Javascript/index.js
+++ 1-Javascript/index.js
@@ -1,4 +1,20 @@
 /*
 JavaScript Nedir?
+JavaScript bir programlama dilidir. Javascript ile birlikte mobil uygulamalar, websiteleri masaüstü uygulamları, akıllı saat uygulamaları 
+gibi uygulamlalar geliştirebiliriz. JavaScript bu nedenle çok önemli ve popüler bir dildir.
+Kullanıcı ile etkileşime girebilen uygulamalar geliştirmek için işimize yarar. Yani probremlerimizi çözebilir.
 
+Nedir bu etkileşim?
+web siteleri ilk çıktığında sadece bir html den ibaretti, kullanıcı ekranında sadece web sitesini görüyordu. Butona basınca yeni bir 
+menünün açılması, kapanması, stillerinin değişmesi gibi interaktif bir halde değildi, sadece görme amaçlı kullanılan bir durumdaydı. 
+Bu duruma web 1.0 denilmektedir.
+
+Web 2.0 ile birlikte kullanıcının herhangi bir butonu olabilir, bu butona tıklanılması sonucu ortaya bir şeyler çıkabiliyor, menü sayfa vs, 
+yani butonumuz aktif olmuş oluyor. 
+Örenğin form doldurup, formu post edebiliyoruz. Bu bir etkileşim örneğidir. 
+Sepetimize ürün ekleme veya çıkarma işlemi, kullanıcıya feedback verebilme işlemi vs etkileşime örnektir. 
+Yukarda saydığımız işlemleri çok güçlü bir şekilde yapmamızı sağlayan bir programlama dilidir.
+
+Browser üzerinde çalışır. Clientside uygulama olarak hayatımıza girdiyse de Node.js in çıkması ile birlikte artık backend tarafında da 
+JavaScript kullanılarak geliştirmeler yapılmaktadır. 
 */
