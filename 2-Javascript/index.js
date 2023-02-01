a7bdc190501ec59b37514da539f21870b89c661a
bilginmrv <mrvblgn44@gmail.com>
2023-02-01 22:28:35 +0300

    Create innerHTML instance

--- 2-Javascript/index.js
+++ 2-Javascript/index.js
@@ -20,3 +20,10 @@ JavaScript tarafında HTML elementlerimize ulaşmamızı sağlayan DOMdur.
   innerText ile div etiketinin içerisindeki metin alındı. Dikkat edilirse burada sadece metni alabiliyoruz. 
   Eğer div’in içerisindeki metni p etiketi ile birlikte almak istiyorsak innerHtml özelliğini kullanılmalıdır.
 */
+
+const title = "Bootcamp From index.js";
+document.querySelector("#title").innerHTML = `<h1>${title}</h1>`;
+/*
+ JavaScripte bazı etiketleri, bazı dom elementlerinin sadece textlerini değiştirmek ile kalmayız, 
+ içerisini bodysini komple değiştirmek istersek veya silmek istersek innerHTML kullanılabilir. 
+*/
