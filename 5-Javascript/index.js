a6efe503a608fa4e7e0093c43f1b1d42450caf39
bilginmrv <mrvblgn44@gmail.com>
2023-02-23 12:59:02 +0300

    ES5 - for loop

--- 5-Javascript/index.js
+++ 5-Javascript/index.js
@@ -18,33 +18,33 @@ const root = document.querySelector("#root");
 //--> döngü kullanarak data arrayimizi html imize yazdıralım
 // arrow fonksiyon tanımı
 const render = () => {
+  // ul tagi oluşturuldu
+  const ul = document.createElement("ul");
 
-    // ul tagi oluşturuldu
-    const ul = document.createElement("ul");
+  // li yi her item için yapılacağı için - döngü
+  // *** ES5
+  for (let i = 0; index < data.length; i++) {
+    const item = data[i];
 
-    // li yi her item için yapılacağı için - döngü 
-    for (let i = 0; index < data.length; i++) {
-        const item = data[i];
+    // her bir data itemi için createElement ile 4 kez li oluşturuldu
+    const li = document.createElement("li"); // oluşturulmuş olan li - leri ul-nin child ına veriyoruz
 
-        // her bir data itemi için createElement ile 4 kez li oluşturuldu
-        const li = document.createElement("li"); // oluşturulmuş olan li - leri ul-nin child ına veriyoruz
+    // linin innerTextine item.name ve item.age verdik
+    // li.innerText = item.name + " " + item.age
+    // --- boşluk bırakma yöntemi ---
+    li.innerText = `${item.name} ${item.age}`;
 
-        // linin innerTextine item.name ve item.age verdik
-        // li.innerText = item.name + " " + item.age
-        // --- boşluk bırakma yöntemi ---
-        li.innerText = `${item.name} ${item.age}`
+    // ul ye li append edilmiş oldu
+    ul.appendChild(li); 
+  }
+  // ul yi html deki root kısmının altında görebilmek için root un append childına ul yi verdik
+  root.appendChild(ul); // tüm işlemleri yapıp bunu root elementine eklememiş olsaydık tüm işlemler havada kalırdı
 
-
-        // ul ye li append edilmiş oldu
-        ul.appendChild(li)
-    }
-    // ul yi html deki root kısmının altında görebilmek için root un append childına ul yi verdik
-    root.appendChild(ul)
-
-    // createElement("ul") - createElement("li") içerisine bir tag alıyor, bu tagi bir dom nesnesi olarak biz js te veriyor, 
-    // daha sonra bu nesneleri programatik olarak htmle eklenileblir
-}
+  // createElement("ul") - createElement("li") içerisine bir tag alıyor, bu tagi bir dom nesnesi olarak biz js te veriyor,
+  // daha sonra bu nesneleri programatik olarak htmle eklenileblir
+};
 render();
-// yukarda oluşturulan render methodu oluşturuldu ve sonrasında çağrıldı, 
+// yukarda oluşturulan render methodu oluşturuldu ve sonrasında çağrıldı,
 // çağrıldıktan sonra render fonksiyonun bulunduğu scoope nin içerisine girer
 
+// Yukarıda verdiğimiz örnek ES6 syntaxında bir for döngüsü değil
