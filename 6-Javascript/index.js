334171c89c2b28e81b42cc4501e1564272f73fab
bilginmrv <mrvblgn44@gmail.com>
2023-02-23 13:05:47 +0300

    ES6 - for loop # 2

--- 6-Javascript/index.js
+++ 6-Javascript/index.js
@@ -21,16 +21,29 @@ const render = () => {
   // ul tagi oluşturuldu
   const ul = document.createElement("ul");
 
-  // ECS 6 for döngüsü #1 * * * * *  * 
-  
+  // ECS 6 for döngüsü #1 * * * * *  *
+  /*
   for (const index in data) {
     const item = data[index];
     const li = document.createElement("li");
     li.innerText = `${item.name} ${item.age}`;
     ul.appendChild(li);
-  } 
+  } */
+
+  for (const item of data) {
+    // of keywordü: ile datanın o anki elementi neyse index numarasına göre hesaplayıp elementini verir
+    // console.log(item);
+
+    const li = document.createElement("li");
+    li.innerText = `${item.name} ${item.age}`;
+    ul.appendChild(li);
+  }
+
+  /*
+    of ile itemin kendisine erişilir
+    in ile indexe erişilir
+  */
 
   root.appendChild(ul); // tüm işlemleri yapıp bunu root elementine eklememiş olsaydık tüm işlemler havada kalırdı
-
 };
 render();
