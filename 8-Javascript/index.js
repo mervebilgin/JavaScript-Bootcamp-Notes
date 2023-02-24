605228e6498059b3542edd408162d783a7a6162d
bilginmrv <mrvblgn44@gmail.com>
2023-02-24 20:41:35 +0300

    Add feature

--- 8-Javascript/index.js
+++ 8-Javascript/index.js
@@ -1,9 +1,8 @@
-
 const data = [
-  { name: "bilge", age: 24 },
-  { name: "merve", age: 22 },
-  { name: "evin", age: 20 },
-  { name: "zeliha", age: 18 },
+  { name: "bilge", age: 24, gender: "kadın" },
+  { name: "hamza", age: 15, gender: "erkek"},
+  { name: "evin", age: 20, gender: "kadın" },
+  { name: "zeliha", age: 18, gender: "kadın" }
 ];
 
 const root = document.querySelector("#root");
@@ -14,15 +13,24 @@ const render = () => {
 
   const renderItem = (item) => {
     const li = document.createElement("li");
-    li.innerText = `${item.name} ${item.age}`;
+    li.innerText = `${item.name} ${item.age} ${item.gender}`;
     ul.appendChild(li);
   };
 
   // FOR-EACH DÖNGÜSÜ
+  // data.forEach((item, index )=> {
+  //   console.log({item, index}); // item: key, index: value
+  // });
+
   data.forEach((item, index )=> {
-    console.log({item, index}); // item: key, index: value
+    console.log(item);
   });
 
   root.appendChild(ul); 
 };
 render();
+
+/*
+  Fonksiyonlar sayesinde birçok kez yapılan , tekrar edilen şeyin merkezi bir 
+  yerden yani tek bir yapılmasını sağlamak için fonksiyonlar bize yardımcı olur.
+*/
