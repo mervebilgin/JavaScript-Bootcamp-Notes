27e3d6d30389daeccd238e789089407de63161dc
bilginmrv <mrvblgn44@gmail.com>
2023-02-25 11:36:47 +0300

    Data listing with fetch

--- 11-Javascript/index.js
+++ 11-Javascript/index.js
@@ -11,25 +11,53 @@
 */
 const data = [
   { name: "bilge", age: 24, gender: "kadın" },
-  { name: "hamza", age: 15, gender: "erkek"},
+  { name: "hamza", age: 15, gender: "erkek" },
   { name: "evin", age: 20, gender: "kadın" },
-  { name: "zeliha", age: 18, gender: "kadın" }
+  { name: "zeliha", age: 18, gender: "kadın" },
 ];
 
 /*
   url bilgimizi her zaman global yerde tutmak isteriz, çünkü birden fazla yerde 
   bu endpoint/ url bilgisine erişmek isteyebilriz.
 */
-const todosUrl = "https://jsonplaceholder.typicode.com/todos"
+const todosUrl = "https://jsonplaceholder.typicode.com/todos";
 const root = document.querySelector("#root");
 
+/*
+Hataları loglamak için kullanılır, api'ler yardımı ile loglayabiliriz,
+bu logları monitör edebilmek için hataları loglamamız gereklidir.
+*/
+const errorLogger = (error) => {
+  console.log({ error });
+};
+
 const renderTodos = () => {
-  // todoları api'lerden al
+  
+  // fetch(todosUrl).then(resp => { // response nesnesi
+  //   console.log(resp)
+  // })
+
   // todoları listele
-  fetch(todosUrl).then(resp => { // response nesnesi
-    console.log(resp)
-  })
-}
+  const ul = document.createElement("ul");
+  const renderItem = (item) => {
+    const li = document.createElement("li");
+    li.innerText = `${item.id} ${item.title} ${item.userId}`;
+    ul.appendChild(li);
+  }
+
+  // todoları api'lerden al
+  fetch(todosUrl)
+    .then(resp => resp.json())
+    .then((todos = []) => {
+      todos.forEach(item => {
+        renderItem(item);
+      });
+      root.appendChild(ul);
+    })
+    .catch(error => {
+      errorLogger(error);
+    });
+};
 /*
   yani json placeholderdan bir requestimiz oldu bize yine response döndü json placeholder apileri
 */
@@ -42,8 +70,4 @@ renderTodos();
   browaerın kendi özelliği olarak eklendi.
   * asenkron çalışır.
 */
-
-const errorLogger = (error) => {
-  console.log({error});
-}
 
